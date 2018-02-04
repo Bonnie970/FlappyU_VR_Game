64333ef462b457e2f96aadfefaed4a207d27ff93
Bonnie970 <guanqing.hu@mail.mcgill.ca>
2018-02-04 10:39:42 -0500

    Main control scripts

---
+++ Vive_Controller.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vive_Controller : MonoBehaviour {
+	private int flapCount = 0;
+	public Transform cameraRigTransform;
+	AudioSource wing;
+	// 1
+	private SteamVR_TrackedObject trackedObj;
+	// 2
+	private SteamVR_Controller.Device Controller
+	{
+		get { return SteamVR_Controller.Input((int)trackedObj.index); }
+	}
+	void Flap(){
+		Vector3 position = cameraRigTransform.position;
+		position.y = (float)0.4;
+		position.x = 0;
+		position.z = 0;
+		cameraRigTransform.position = cameraRigTransform.position + position;
+
+		wing.Play ();
+
+	}
+	void Awake()
+	{
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+		wing = GetComponent<AudioSource>();
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Controller.velocity [1] < -1) {
+			flapCount += 1;
+			if (Vive_player.inGame == true) {
+				Flap ();
+			}
+		}
+		if (Controller.GetHairTriggerDown ()) {
+			Vive_player.inGame = true;
+		}
+		//Debug.Log(gameObject.name + Controller.velocity[1]);
+	}
+}
---
+++ Vive_player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Vive_player : MonoBehaviour {
+	public static bool inGame = false;
+	public Transform cameraRigTransform;
+	public static int score = 0;
+	public GameObject textObject;
+	public TextMesh startText;
+
+	void Start(){
+		startText = textObject.GetComponent<TextMesh> ();
+		startText.text = "FlapVR\nPress trigger to Start \nScore: " + score;
+	}
+	void OnTriggerEnter (Collider other)
+	{
+		inGame = false;
+		Debug.Log ("Collision");
+	}
+	// Update is called once per frame
+	void Update () {
+		if (!inGame) {
+			this.GetComponent<Rigidbody> ().useGravity = false;
+			cameraRigTransform.position = new Vector3 (0, 10, 0);
+			startText.text = "FlapVR\nPress trigger to Start \nScore: " + score;
+		} else {
+			this.GetComponent<Rigidbody> ().useGravity = true;
+			startText.text = "Score: " + score;
+		}
+
+	}
+}
